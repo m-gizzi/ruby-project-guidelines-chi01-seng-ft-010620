76d815afaa45c20256172e23e37b0a34b7a76915
github username <github email address>
2020-01-22 14:38:25 -0600

    Upload & Homepage

--- bin/run.rb
+++ bin/run.rb
@@ -1,18 +1,21 @@
 require_relative '../config/environment'
+require_relative 'upload'
 require 'pry'
 
 system "clear"
 puts "Welcome to Concertgram!\n\n"
 puts "Please enter a username"
 user_name = gets.chomp
-User.find_or_create_by_name(user_name)
+USER = User.find_or_create_by_name(user_name)
 system "clear"
+
 def homepage(user_name)
     puts "Hi #{user_name}! Please select an option:\n\n"
     puts "1. Upload photo"
     puts "2. Random photo"
     puts "3. My photos"
     puts "4. Search"
+    puts "5. Quit"
 
 
     selection = gets.chomp
@@ -23,18 +26,23 @@ def homepage(user_name)
 
     case selection
     when "1"
-        upload
+        upload(user_name)
     when "2"
         random_page
     when "3"
-        my_photos
+        my_photos(user_name)
     when "4"
         search
+    when "5"
+        puts "Thank you for using Concertgram"
+        exit
     else 
         homepage(user_name)
     end
 end
 homepage(user_name)
+
+
 #ascii = ASCII_Image.new("http://www.levihackwith.com/wp-content/uploads/2011/10/github-logo.png")
 #ascii.build(60)
 
--- bin/upload.rb
+++ bin/upload.rb
@@ -1,17 +1,22 @@
+
 def upload(user_name)
     puts "Submit your photo url or press q to go back to the home page"
-    selection = gets.chomp
-    case selection
+    photo_url = gets.chomp
+    case photo_url
     when "q"
         homepage(user_name)
     else 
         puts "Enter the name of the concert the photo is of"
         concert_name = gets.chomp
-        if concert_name
+        concert = Concert.find_by(displayName: concert_name)
+        if concert
             puts "Caption?"
             caption = gets.chomp
+            USER.upload_photo(photo_url, concert, caption)
 
             puts "Upload Complete"
+            
+            
             
         else
             puts "Concert not found please reupload your photo"
@@ -19,3 +24,6 @@ def upload(user_name)
         end
     end
 end
+
+
+require_relative 'run'
