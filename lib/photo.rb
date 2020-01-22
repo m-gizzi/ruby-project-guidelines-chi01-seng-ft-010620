8f2601f233054c5e707fdb6fd0e204fbb3aa0054
m-gizzi <matthewgizzi@verizon.net>
2020-01-22 11:21:17 -0600

    Method fixes

--- lib/photo.rb
+++ lib/photo.rb
@@ -1,13 +1,16 @@
+require 'pry'
+
 class Photo < ActiveRecord::Base
     belongs_to :user
     belongs_to :concert
     
     def self.photos_by_concert_name(name)
-        self.all.select{|photo| photo.concert.name == name}
+        self.all.select{|photo| photo.concert.displayName == name}
     end
 
-    def self.photos_by_date(date)
-        self.all.select{|photo| photo.created_at.to_date == date}
+    def self.photos_by_date(date_as_string)
+        validated = date_as_string.to_date
+        self.all.select{|photo| photo.created_at.to_date == validated}
     end
 
     def self.random_photo
